803df7849d15a6bdabac859deaa96ebc9607ec6c
Steven Chin <chin.steven10@gmail.com>
2022-10-30 23:48:06 -0700

    Update paths

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -15,22 +15,22 @@ function init() {
   hornSelect.addEventListener("input", e => {
     switch (e.target.value) {
       case 'air-horn':
-        hornImage.src = "/assets/images/air-horn.svg";
+        hornImage.src = "../images/air-horn.svg";
         hornImage.alt = "air horn";
-        audio.src = "/assets/audio/air-horn.mp3";
+        audio.src = "../audio/air-horn.mp3";
         break;
       case 'car-horn':
-        hornImage.src = "/assets/images/car-horn.svg";
+        hornImage.src = "../images/car-horn.svg";
         hornImage.alt = "car horn";
-        audio.src = "/assets/audio/car-horn.mp3";
+        audio.src = "../audio/car-horn.mp3";
         break;
       case 'party-horn':
-        hornImage.src = "/assets/images/party-horn.svg";
+        hornImage.src = "../images/party-horn.svg";
         hornImage.alt = "party horn";
-        audio.src = "/assets/audio/party-horn.mp3";
+        audio.src = "../audio/party-horn.mp3";
         break;
       default:
-        hornImage.src = "/assets/images/no-image.png";
+        hornImage.src = "../images/no-image.png";
         hornImage.alt = "no image selected";
         audio.src = "";
         break;
@@ -40,19 +40,19 @@ function init() {
   volume.addEventListener("input", e => {
     const volumeLevel = e.target.value;
     if (volumeLevel == 0) {
-      volumeImage.src = "/assets/icons/volume-level-0.svg";
+      volumeImage.src = "../icons/volume-level-0.svg";
       volumeImage.alt = "volume level 0";
     } else if ((1 <= volumeLevel) && (volumeLevel < 33)) {
-      volumeImage.src = "/assets/icons/volume-level-1.svg";
+      volumeImage.src = "../icons/volume-level-1.svg";
       volumeImage.alt = "volume level 1";
     } else if ((33 <= volumeLevel) && (volumeLevel < 67)) {
-      volumeImage.src = "/assets/icons/volume-level-2.svg";
+      volumeImage.src = "../icons/volume-level-2.svg";
       volumeImage.alt = "volume level 2";
     } else if ((67 <= volumeLevel) && (volumeLevel <= 100)) {
-      volumeImage.src = "/assets/icons/volume-level-3.svg";
+      volumeImage.src = "../icons/volume-level-3.svg";
       volumeImage.alt = "volume level 3";
     } else {
-      volumeImage.src = "/assets/images/no-image.png";
+      volumeImage.src = "../images/no-image.png";
       volumeImage.alt = "no image";
     }
 
